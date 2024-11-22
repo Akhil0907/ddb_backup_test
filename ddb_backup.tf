e10c2c59ab53ff6d28e906a430eada2297472147
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2024-11-22 14:56:47 +0530

    Update ddb_backup.tf

--- ddb_backup.tf
+++ ddb_backup.tf
@@ -1,46 +1,47 @@
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
-  profile = "akhil-mfa"
+  region = var.aws_region
+  default_tags {
+    tags = {
+      terraform = true
+    }
+  }
 }
 
-provider "random" {
-  # No configuration needed for the random provider
-}
-
-provider "local" {
-  # No configuration needed for the local provider
-}
-
-resource "random_string" "hash_key" {
-  length  = 16
-  special = false
-}
-
-resource "aws_dynamodb_table" "content" {
-  name         = var.table_name
+resource "aws_dynamodb_table" "entitlements_ledger_metadata" {
+  name         =  var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "pk"  # Partition key
-  range_key    = "sk"  # Sort key
+  hash_key     = "pk"
+  range_key    = "sk"
 
   attribute {
     name = "pk"
     type = "S"
   }
-
   attribute {
     name = "sk"
-    type = "N"
+    type = "S"
   }
-
-
+  tags = {
+    terraform   = true
+    environment = var.environment_name
+  }
   point_in_time_recovery {
     enabled = true
   }
-
   lifecycle {
-    ignore_changes = [
-      name,
-      replica
-    ]
+    ignore_changes = [name, replica]
   }
 }
