4a5d25a4833c606d84dbacae06f044d31f950ac4
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2024-12-02 19:04:47 +0530

    Update ddb_backup.tf

--- ddb_backup.tf
+++ ddb_backup.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     region = "us-east-1"
+    bucket = "sandbox-tf-test"
+    key = "tfstate/sandbox/devops.tfstate"
   }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
